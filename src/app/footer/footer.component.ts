1a974c4e5bada294517ab372b5c9200007035e5c
Alan Nugent <alan@alannugent.com>
2017-05-30 16:57:30 -0400

    adding footer to footer folder

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'et-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer/_footer.component.scss']
+  styleUrls: ['./_footer.component.scss']
 })
 export class FooterComponent {
   title = 'app works!';
