88e0185ec307103c76c9810f8f83b4433e1ea29f
Alan <alan@alannugent.com>
2017-06-05 21:53:56 -0400

    adding variables and jasny

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'et-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./_footer.component.scss']
-})
+  templateUrl: './footer.component.html'
+  })
 export class FooterComponent {
   title = 'app works!';
 }
