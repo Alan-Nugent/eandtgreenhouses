5cba9e74c8c91f3367aeb33cc396710a992a22ec
Alan Nugent <alan@alannugent.com>
2017-08-11 20:44:26 -0400

    adding function to close menu on page change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,44 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router, NavigationEnd} from '@angular/router';
 import {GoogleAnalyticsEventsService} from './google-analytics-events.service';
 
 
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  title = 'E&T Greenhouses';
+export class AppComponent implements OnInit {
+    title = 'E&T Greenhouses';
 
-  constructor(public router: Router, public googleAnalyticsEventsService: GoogleAnalyticsEventsService) {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-         //ga('set', 'page', event.urlAfterRedirects);
-          //ga('send', 'pageview');
-      }
-    });
-  }
+    constructor(public router: Router, public googleAnalyticsEventsService: GoogleAnalyticsEventsService) {
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+            }
+        });
+    }
 
-  submitEvent() {
-    this.googleAnalyticsEventsService.emitEvent(
-      'testCategory',
-      'testAction',
-      'testLabel',
-      10
-    );
-  }
+    ngOnInit() {
+        this.router.events.subscribe((evt) => {
+            if (!(evt instanceof NavigationEnd)) {
+                return;
+            }
+            window.scrollTo(0, 0);
+        });
+    }
 
-  onDeactivate() {
-    document.body.scrollTop = 0;
-  }
+
+    submitEvent() {
+        this.googleAnalyticsEventsService.emitEvent(
+            'testCategory',
+            'testAction',
+            'testLabel',
+            10
+        );
+    }
+
+    onDeactivate() {
+        document.body.scrollTop = 0;
+    }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'et-header',
-  templateUrl: './header.component.html',
-  host:{
-      'class': 'et-header'
-  }
- // styleUrls: ['./_header.component.scss']
+    selector: 'et-header',
+    templateUrl: './header.component.html',
+    host: {
+        'class': 'et-header'
+    }
+    // styleUrls: ['./_header.component.scss']
 })
 
 
 export class HeaderComponent {
-  opened = false;
-  clicked(sidenav) {
-    sidenav.open();
-    this.opened = !this.opened;
-  }
+    opened = false;
 
+    clicked(sidenav) {
+        sidenav.open();
+        this.opened = !this.opened;
+    }
+
+    closeNav(sidenav) {
+        sidenav.close();
+        this.opened = !this.opened;
+    }
 }
